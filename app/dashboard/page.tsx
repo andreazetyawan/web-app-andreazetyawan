d5e03ca50707307d63badae03abb5af1d8f4942c
andreazetyawan <andreazetyawan@andreazetyawan.id>
2024-06-08 04:32:40 -0400

    test api client route

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,25 @@
-import { useState, useEffect } from 'react'
+'use client'
+
+import { useState, useEffect } from "react"
  
 export default function Dashboard() {
   const [data, setData] = useState(null)
   const [isLoading, setLoading] = useState(true)
  
   useEffect(() => {
-    fetch('https://api.andreazetyawan.id/v1/')
+    fetch('https://api.andreazetyawan.id/v1', {
+      method: 'GET',  
+      headers: {
+        'Content-Type': 'application/json'
+        // Add any other headers as needed for your application
+      },
+    })
       .then((res) => res.json())
       .then((data) => {
         setData(data)
         setLoading(false)
       })
+      .catch(error => console.error('Error:', error));
   }, [])
  
   if (isLoading) return <p>Loading...</p>
