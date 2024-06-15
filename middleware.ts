9d3d0e40f30d43dabb9ea317a992dc87c37ec2a3
andreazetyawan <andreazetyawan@andreazetyawan.id>
2024-06-15 19:12:34 -0400

    disable api config

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { verifyRequestOrigin } from "lucia";
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(request: NextRequest): Promise<NextResponse> {
+	if (request.method === "GET") {
+		return NextResponse.next();
+	}
+	
+	// NOTE: You may need to use `X-Forwarded-Host` instead
+  	const host = request.headers.get('host');
+  	const origin = request.headers.get('origin');
+
+	if (!origin || !host || !verifyRequestOrigin(origin, [host,"andreazetyawan.id","www.andreazetyawan.id","api.andreazetyawan.id"])) {
+		return new NextResponse(null, {
+			status: 403
+		});
+	}
+	return NextResponse.next();
+}
+
+// export const config = {
+//   matcher: '/api/:path*',
+// }
